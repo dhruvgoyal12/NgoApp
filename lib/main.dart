25948f6f1f02eea9bd86a61b40da356344f1254b
Muskan Garg <52607596+MuskanGarg07@users.noreply.github.com>
2020-04-25 00:11:07 +0530

    .

--- lib/main.dart
+++ lib/main.dart
@@ -64,7 +64,7 @@ class _MyAppState extends State<MyApp> {
       theme: ThemeData(
         primaryColor: Colors.teal,
       ),
-      title: 'NGO app',
+      title: 'NGGGGGGGO app',
       routes: <String, WidgetBuilder>{
         '/choice': (BuildContext context) => new choice(),
         '/login': (BuildContext context) => new login(widget.cam),
